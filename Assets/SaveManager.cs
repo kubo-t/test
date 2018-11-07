625048f3b6a3feb3c13583c8e2d671b8684173e9
kubotakuya <kbtk8427@gmail.com>
2018-11-07 21:39:05 +0900

    1107_____

---
+++ Assets/SaveManager.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System; // UnityJsonを使う場合に必要
+using System.IO; // ファイル書き込みに必要
+
+// [Serializable] をつけないとシリアライズできない
+[Serializable]
+public struct SaveData {
+
+	//public int stageNum;
+
+	//public bool[] clearedStage = new bool[8];
+	public bool[] clearedStage;
+
+	//public int x;
+	//public int y;
+
+	public void Dump() {
+		//Debug.Log("x = " + x);
+		//Debug.Log("y = " + y);
+
+		//Debug.Log (stageNum);
+	}
+}
+
+/// <summary>
+/// セーブデータ管理
+/// </summary>
+public class SaveManager : MonoBehaviour {
+
+	public int stageNum;
+	public bool[] clearedStage = {false,false,false,false,false,false,false,false};
+
+	//private int totalStageNum;
+
+	// 保存するファイル
+	const string SAVE_FILE_PATH = "save.txt";
+
+	void Start(){
+		var data = new SaveData ();
+
+		//data.stageNum = this.stageNum;
+		//totalStageNum = clearedStage.Length;
+
+
+		//for(int i = 0;i <= this.stageNum;i++){
+		//	data.clearedStage [i] = true;
+		//}
+
+		var info = new FileInfo(Application.dataPath + "/" + SAVE_FILE_PATH);
+		var reader = new StreamReader (info.OpenRead ());
+		var jsonLoad = reader.ReadToEnd ();
+		var dataLoad = JsonUtility.FromJson<SaveData>(jsonLoad);
+
+		data.clearedStage = new bool[this.clearedStage.Length]; //clearedStageを初期化
+
+		for(int i = 0;i < this.clearedStage.Length;i++){
+			data.clearedStage [i] = dataLoad.clearedStage [i]; //clearedStageにsave.txtからLoadしたものを入れる
+		}
+
+		reader.Close ();
+
+
+		data.clearedStage [this.stageNum] = true; // claredStage[現在のステージ]をtrueにして　↓save
+
+		for(int i = 0;i < this.clearedStage.Length;i++){ // ここまではちゃんと動くけど
+			Debug.Log (data.clearedStage[i]);
+		}
+			
+		// JSONにシリアライズ
+		var json = JsonUtility.ToJson (data);
+		// Assetsフォルダに保存する
+		var path = Application.dataPath + "/" + SAVE_FILE_PATH;
+		var writer = new StreamWriter (path, false); // 上書き
+		writer.WriteLine (json);
+		writer.Flush ();
+		writer.Close ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.S)) {
+			
+			// Sキーでセーブ実行
+			//var data = new SaveData ();
+			//data.x = 5;
+			//data.y = 7;
+			//data.clearStageNum = this.clearStageNum;
+			// JSONにシリアライズ
+			//var json = JsonUtility.ToJson (data);
+			// Assetsフォルダに保存する
+			//var path = Application.dataPath + "/" + SAVE_FILE_PATH;
+			//var writer = new StreamWriter (path, false); // 上書き
+			//writer.WriteLine (json);
+			//writer.Flush ();
+			//writer.Close ();
+
+		} else if (Input.GetKeyDown (KeyCode.L)) {
+			// Lキーでロード実行
+			// Assetsフォルダからロード
+
+			var info = new FileInfo(Application.dataPath + "/" + SAVE_FILE_PATH);
+			var reader = new StreamReader (info.OpenRead ());
+			var json = reader.ReadToEnd ();
+			var data = JsonUtility.FromJson<SaveData>(json);
+			data.Dump();
+
+			for(int i = 0;i < this.clearedStage.Length;i++){ // ここで入ってない？？？？？？？ Saveミスってそう
+				Debug.Log (data.clearedStage[i]);
+			}
+
+		}
+	}
+
+	public void DataLoad(){
+		var info = new FileInfo(Application.dataPath + "/" + SAVE_FILE_PATH);
+		var reader = new StreamReader (info.OpenRead ());
+		var json = reader.ReadToEnd ();
+		var data = JsonUtility.FromJson<SaveData>(json);
+		data.Dump();
+
+		for(int i = 0;i < this.clearedStage.Length;i++){
+			this.clearedStage [i] = data.clearedStage [i];
+		}
+		//this.stageNum = data.stageNum;
+		//Debug.Log (this.stageNum);
+	}
+}
---
+++ Assets/Stages/SceneManager/SelectMenu.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectMenu : MonoBehaviour {
+	
+    Button stage1;
+    Button stage2;
+    Button stage3;
+    Button stage4;
+    Button stage5;
+    Button stage6;
+	Button stage7;
+
+	SaveManager saveManager;
+
+    // Use this for initialization
+    void Start () {
+        stage1 = GameObject.Find("/Canvas/Button1").GetComponent<Button>();
+        stage2 = GameObject.Find("/Canvas/Button2").GetComponent<Button>();
+        stage3 = GameObject.Find("/Canvas/Button3").GetComponent<Button>();
+        stage4 = GameObject.Find("/Canvas/Button4").GetComponent<Button>();
+        stage5 = GameObject.Find("/Canvas/Button5").GetComponent<Button>();
+        stage6 = GameObject.Find("/Canvas/Button6").GetComponent<Button>();
+		stage7 = GameObject.Find("/Canvas/Button7").GetComponent<Button>();
+
+		saveManager = GameObject.Find ("DataLoad").GetComponent<SaveManager>();
+		saveManager.DataLoad ();
+
+		stage1.enabled = false;
+		stage2.enabled = false;
+		stage3.enabled = false;
+		stage4.enabled = false;
+		stage5.enabled = false;
+		stage6.enabled = false;
+		stage7.enabled = false;
+
+		stage1.image.color = Color.gray;
+		stage2.image.color = Color.gray;
+		stage3.image.color = Color.gray;
+		stage4.image.color = Color.gray;
+		stage5.image.color = Color.gray;
+		stage6.image.color = Color.gray;
+		stage7.image.color = Color.gray;
+
+		if(saveManager.clearedStage[1] == true){
+			stage1.enabled = true;
+			stage1.image.color = Color.white;
+		}
+		if(saveManager.clearedStage[2] == true){
+			stage2.enabled = true;
+			stage2.image.color = Color.white;
+		}
+		if(saveManager.clearedStage[3] == true){
+			stage3.enabled = true;
+			stage3.image.color = Color.white;
+		}
+		if(saveManager.clearedStage[4] == true){
+			stage4.enabled = true;
+			stage4.image.color = Color.white;
+		}
+		if(saveManager.clearedStage[5] == true){
+			stage5.enabled = true;
+			stage5.image.color = Color.white;
+		}
+		if(saveManager.clearedStage[6] == true){
+			stage6.enabled = true;
+			stage6.image.color = Color.white;
+		}
+		if(saveManager.clearedStage[7] == true){
+			stage7.enabled = true;
+			stage7.image.color = Color.white;
+		}
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
